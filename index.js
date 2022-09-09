43f4ac2c084e4357014359ecd5950507dc4730e4
Mohammad Anas Siddiquei <mohaasid@amazon.com>
2022-09-10 04:17:32 +0530

    Added notification rule for email

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World. Test project with aws code pipeline. SNS notification added\n'
+  const msg = 'Hello World. Test project with aws code pipeline. SNS notification added. Email notification added.\n'
   res.end(msg);
 });
 
