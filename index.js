e8bb0ee9df7c306c239fba64944e572cae4468c4
Mohammad Anas Siddiquei <mohaasid@amazon.com>
2022-09-11 17:43:34 +0530

    test Deploye

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World. Test project with aws code pipeline. SNS notification added. Email notification added.\n'
+  const msg = 'Hello World. Test project with aws code pipeline. SNS notification added. Email notification added. Test Deploy\n'
   res.end(msg);
 });
 
